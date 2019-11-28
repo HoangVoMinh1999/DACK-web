afe6146ee7e6ab71758300b6481dbd9b99f6252d
Vo Minh Hoang <hoangvm.hiephoa@gmail.com>
2019-11-29 03:55:25 +0700

    fix db not null

--- models/products.js
+++ models/products.js
@@ -4,6 +4,7 @@ var mongoose=require('mongoose');
 var express=require('express');
 var router = express.Router();
 const assert = require('assert');
+const mongodb=require('mongodb');
 const MongoClient = require('mongodb').MongoClient;
 var uri = process.env.DB_LOCALHOST || process.env.DB_ATLAS;
 const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true });
