d6276075c78caf686c36df8923e5432891adee91
Vo Minh Hoang <hoangvm.hiephoa@gmail.com>
2019-11-29 13:54:53 +0700

    add imagePath

--- myDatabase/products.js
+++ myDatabase/products.js
@@ -12,7 +12,8 @@ const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: 
 
 ///---Database of Shirt
 
-function data(id,name,brand,type,price){
+function data(imagePath,id,name,brand,type,price){
+    this.imagePath=imagePath;
     this.id=id;
     this.name=name;
     this.brand=brand;
@@ -22,6 +23,7 @@ function data(id,name,brand,type,price){
 
 var data_products=[
     new data(
+        "https://www.viettien.com.vn/admin/wp-content/uploads/2018/11/3a-1.jpg",
         "S_01",
         "viettien_01",
         "Việt Tiến",
@@ -29,6 +31,7 @@ var data_products=[
         1000000
     ),
     new data(
+        "https://www.viettien.com.vn/admin/wp-content/uploads/2018/11/3a-1.jpg",
         "S_01",
         "viettien_02",
         "Việt Tiến",
@@ -36,6 +39,7 @@ var data_products=[
         500000
     ),
     new data(
+        "https://www.viettien.com.vn/admin/wp-content/uploads/2018/11/3a-1.jpg",
         "S_03",
         "viettien_03",
         "Việt Tiến",
@@ -43,6 +47,7 @@ var data_products=[
         45678
     ),
     new data(
+        "https://assets.adidas.com/images/w_600,f_auto,q_auto:sensitive,fl_lossy/d285610e30664900b857a7fa00ed0201_9366/Superstar_Shoes_White_C77124_01_standard.jpg",
         "Sh_01",
         "Adidas_01",
         "Adidas",
@@ -50,6 +55,7 @@ var data_products=[
         1000000
     ),
     new data(
+        "https://assets.adidas.com/images/w_600,f_auto,q_auto:sensitive,fl_lossy/d285610e30664900b857a7fa00ed0201_9366/Superstar_Shoes_White_C77124_01_standard.jpg",
         "Sh_01",
         "Adidas_02",
         "Adidas",
@@ -57,6 +63,7 @@ var data_products=[
         500000
     ),   
     new data(
+        "https://c.static-nike.com/a/images/t_PDP_1280_v1/f_auto/gorfwjchoasrrzr1fggt/air-max-270-shoe-nnTrqDGR.jpg",
         "Sh_03",
         "Nike_03",
         "Nike",
@@ -64,6 +71,7 @@ var data_products=[
         345678
     ),
     new data(
+        "https://s7d5.scene7.com/is/image/UrbanOutfitters/52145463_106_f?$xlarge$&hei=900&qlt=80&fit=constrain",
         "T_01",
         "Levis_011",
         "Levis",
@@ -71,6 +79,7 @@ var data_products=[
         1000000
     ),
     new data(
+        "https://s7d5.scene7.com/is/image/UrbanOutfitters/52145463_106_f?$xlarge$&hei=900&qlt=80&fit=constrain",
         "T_01",
         "Levis_012",
         "Levis",
@@ -78,6 +87,7 @@ var data_products=[
         500000
     ),
     new data(
+        "https://s7d5.scene7.com/is/image/UrbanOutfitters/52145463_106_f?$xlarge$&hei=900&qlt=80&fit=constrain",
         "T_03",
         "Levis_013",
         "Levis",
